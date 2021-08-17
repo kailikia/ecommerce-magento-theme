d4b03a93e5a549ba86e153c5935dc81c3430b211
kailikia <edwinkailikia@gmail.com>
2021-08-17 13:23:58 +0300

    initial front

---
+++ app/code/Codazon/SalesPro/view/frontend/web/js/checkout/model/checkout-data-resolver-mixin.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright © Codazon, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'jquery',
+    'Magento_Customer/js/model/address-list',
+    'Magento_Checkout/js/model/quote',
+    'Magento_Checkout/js/checkout-data',
+    'Magento_Checkout/js/action/create-shipping-address',
+    'Magento_Checkout/js/action/select-shipping-address',
+    'Magento_Checkout/js/action/select-shipping-method',
+    'Magento_Checkout/js/model/payment-service',
+    'Magento_Checkout/js/action/select-payment-method',
+    'Magento_Checkout/js/model/address-converter',
+    'Magento_Checkout/js/action/select-billing-address',
+    'Magento_Checkout/js/action/create-billing-address',
+    'underscore',
+    'mage/utils/wrapper'
+], function ($,
+    addressList,
+    quote,
+    checkoutData,
+    createShippingAddress,
+    selectShippingAddress,
+    selectShippingMethodAction,
+    paymentService,
+    selectPaymentMethodAction,
+    addressConverter,
+    selectBillingAddress,
+    createBillingAddress,
+    _,
+    wrapper) {
+    'use strict';
+    return function(widget) {
+        
+        return widget;
+    }
+});
---
+++ app/code/Codazon/SalesPro/view/frontend/web/js/checkout/view/billing-address-mixin.js
@@ -0,0 +1,82 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'ko',
+    'underscore',
+    'Magento_Ui/js/form/form',
+    'Magento_Customer/js/model/customer',
+    'Magento_Customer/js/model/address-list',
+    'Magento_Checkout/js/model/quote',
+    'Magento_Checkout/js/action/create-billing-address',
+    'Magento_Checkout/js/action/select-billing-address',
+    'Magento_Checkout/js/checkout-data',
+    'Magento_Checkout/js/model/checkout-data-resolver',
+    'Magento_Customer/js/customer-data',
+    'Magento_Checkout/js/action/set-billing-address',
+    'Magento_Ui/js/model/messageList',
+    'mage/translate',
+    'Magento_Checkout/js/model/billing-address-postcode-validator'
+],
+function (
+    ko,
+    _,
+    Component,
+    customer,
+    addressList,
+    quote,
+    createBillingAddress,
+    selectBillingAddress,
+    checkoutData,
+    checkoutDataResolver,
+    customerData,
+    setBillingAddressAction,
+    globalMessageList,
+    $t,
+    billingAddressPostcodeValidator
+) {
+    var lastSelectedBillingAddress = null,
+        countryData = customerData.get('directory-data'),
+        addressOptions = addressList().filter(function (address) {
+            return address.getType() === 'customer-address';
+        });
+    return function (Component) {
+        return Component.extend({
+            /**
+             * @return {exports.initObservable}
+             */
+            initObservable: function () {
+                this._super()
+                    .observe({
+                        selectedAddress: null,
+                        isAddressDetailsVisible: quote.billingAddress() != null,
+                        isAddressFormVisible: !customer.isLoggedIn() || !addressOptions.length,
+                        isAddressSameAsShipping: false,
+                        saveInAddressBook: 1
+                    });
+
+                quote.billingAddress.subscribe(function (newAddress) {
+                    if (quote.isVirtual() || !quote.shippingAddress()) {
+                        this.isAddressSameAsShipping(false);
+                    } else {
+                        this.isAddressSameAsShipping(
+                            newAddress != null &&
+                            newAddress.getCacheKey() == quote.shippingAddress().getCacheKey() //eslint-disable-line eqeqeq
+                        );
+                    }
+
+                    if (newAddress != null && newAddress.saveInAddressBook !== undefined) {
+                        this.saveInAddressBook(newAddress.saveInAddressBook);
+                    } else {
+                        this.saveInAddressBook(1);
+                    }
+                    this.isAddressDetailsVisible(true);
+                }, this);
+
+                return this;
+            }
+        });
+    }
+});
